e58c5c417e262fff5891fb32f93a029e5920ab63
onsen-egg <work-account-root@protonmail.com>
2022-05-01 08:58:11 -0500

    Added Gasometer with dummy method implementations

---
+++ contracts/terranova/src/gasometer.rs
@@ -0,0 +1,61 @@
+use std::convert::TryInto;
+
+use evm::{U256, H160};
+
+use crate::{storage::StorageInterface, executor_state::ExecutorState, transaction::UnsignedTransaction};
+
+const LAMPORTS_PER_SIGNATURE: u64 = 5000;
+
+/// THIS IS A DUMMY TO MAKE FUNCTION CALLS WORK
+pub struct Gasometer {
+    gas: u64,
+}
+
+impl Gasometer {
+    pub fn new() -> Self {
+        Self { gas: 0_u64 }
+    }
+
+    #[must_use]
+    pub fn used_gas(&self) -> U256 {
+        U256::from(self.gas)
+    }
+
+    /// DUMMY
+    pub fn record_iterative_overhead(&mut self) {
+
+    }
+
+    pub fn record_transaction_size(&mut self, trx: &UnsignedTransaction) {
+
+    }
+
+    pub fn record_evm_steps(&mut self, steps: u64) {
+
+    }
+
+    pub fn pad_evm_steps(&mut self, steps: u64) {
+
+    }
+
+    pub fn record_storage_write<B>(&mut self, state: &ExecutorState<B>, address: H160, key: U256)
+    where
+        B: StorageInterface
+    {
+
+    }
+
+    pub fn record_deploy<B>(&mut self, state: &ExecutorState<B>, address: H160)
+    where
+        B: StorageInterface
+    {
+
+    }
+
+    pub fn record_transfer<B>(&mut self, state: &ExecutorState<B>, target: H160, value: U256)
+    where
+        B: StorageInterface
+    {
+
+    }
+}
---
+++ contracts/terranova/src/transaction.rs
@@ -0,0 +1,13 @@
+use evm::{H160, U256};
+
+#[derive(Debug)]
+pub struct UnsignedTransaction {
+    pub nonce: u64,
+    pub gas_price: U256,
+    pub gas_limit: U256,
+    pub to: Option<H160>,
+    pub value: U256,
+    pub call_data: Vec<u8>,
+    pub chain_id: Option<U256>,
+    pub rlp_len: usize,
+}
