42b22345def8edcd1ba782cad82a0a7844f8bcf1
onsen-egg <work-account-root@protonmail.com>
2022-05-06 14:57:39 -0500

    Implemented chunked transaction sending

--- contracts/terranova/src/message/execute_chunked_transaction.rs
+++ contracts/terranova/src/message/execute_chunked_transaction.rs
@@ -2,24 +2,22 @@ use cosmwasm_std::{DepsMut, Env, Response, Storage};
 use evm::{H160, H256};
 
 use crate::ContractError;
+use crate::tx_chunk;
+use crate::utils::keccak256_h256;
+
+use super::execute_simple_transaction;
 
 pub fn process(deps: DepsMut, env: Env, caller_address_bytes: [u8; 20], full_tx_hash: [u8; 32], chunk_count: u8) -> Result<Response, ContractError> {
-    todo!();
+    let caller_address = H160::from_slice(&caller_address_bytes);
+    let full_tx_hash = H256::from_slice(&full_tx_hash);
+
+    let collected_tx_bytes = tx_chunk::collect_chunks(deps.as_ref(), caller_address, full_tx_hash, chunk_count)?;
     
-    let caller_address_h160 = H160::from_slice(&caller_address_bytes);
-    let tx_hash = H256::from_slice(&full_tx_hash);
+    // Check that the hash of the merged transaction chunks match the provided transaction hash
+    if full_tx_hash != keccak256_h256(&collected_tx_bytes) {
+        return Err(ContractError::InvalidTxChunks)
+    }
 
-    validate()?;
-
-    execute(deps.storage, caller_address_h160, tx_hash, chunk_count)
-}
-
-pub fn validate() -> Result<(), ContractError> {
-    Ok(())
-}
-
-pub fn execute(storage: &mut dyn Storage, caller_address: H160, tx_hash: H256, chunk_count: u8) -> Result<Response, ContractError> {
-    let response = Response::new();
-
-    Ok(response)
+    // Validation and execution will be handled in execute_simple_transaction
+    execute_simple_transaction::process(deps, env, caller_address_bytes, collected_tx_bytes)
 }
--- contracts/terranova/src/message/store_transaction_chunk.rs
+++ contracts/terranova/src/message/store_transaction_chunk.rs
@@ -2,24 +2,26 @@ use cosmwasm_std::{DepsMut, Env, Response, Storage};
 use evm::{H160, H256};
 
 use crate::ContractError;
+use crate::tx_chunk;
 
 pub fn process(deps: DepsMut, caller_address_bytes: [u8; 20], full_tx_hash: [u8; 32], chunk_index: u8, chunk_data: Vec<u8>) -> Result<Response, ContractError> {
-    todo!();
-
     let caller_address = H160::from_slice(&caller_address_bytes);
     let tx_hash = H256::from_slice(&full_tx_hash);
 
     validate()?;
 
-    execute(deps.storage, caller_address, tx_hash, chunk_index, chunk_data)
+    execute(deps, caller_address, tx_hash, chunk_index, chunk_data)
 }
 
 pub fn validate() -> Result<(), ContractError> {
     Ok(())
 }
 
-pub fn execute(storage: &mut dyn Storage, caller_address: H160, tx_hash: H256, chunk_index: u8, chunk_data: Vec<u8>) -> Result<Response, ContractError> {
-    let response = Response::new();
+pub fn execute(deps: DepsMut, caller_address: H160, tx_hash: H256, chunk_index: u8, chunk_data: Vec<u8>) -> Result<Response, ContractError> {
+    tx_chunk::write_chunk(deps, caller_address, tx_hash, chunk_index, chunk_data)?;
+
+    let response = Response::new()
+        .add_attribute("chunk_index", chunk_index.to_string());
 
     Ok(response)
 }
